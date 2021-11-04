832c3df4663fb54222f1f9dfa48cf787ee83bb96
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-11-04 17:21:52 +0100

    Create main function

---
+++ convert.go
@@ -0,0 +1,28 @@
+//********************************************************************************************************************//
+//
+// This file is part of spechar.
+// All code may be used. Feel free and maybe code something better.
+//
+// Author: Jonas Kwiedor (aka gowizzard)
+//
+//********************************************************************************************************************//
+
+package spechar
+
+import "strings"
+
+// Convert is to convert the string & remove all special characters
+func Convert(text string) string {
+
+	// Create list for special characters
+	list := []string{"!", "\"", "§", "$", "%", "&", "/", "(", ")", "=", "ß", "[", "]", "|", "{", "}", "<", ">"}
+
+	// Check each special characters & replace ti with nothing
+	for _, value := range list {
+		text = strings.Replace(text, value, "", -1)
+	}
+
+	// Return text without umlauts
+	return text
+
+}
